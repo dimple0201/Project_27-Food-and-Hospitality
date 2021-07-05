79bac08a9f27bdbcded0c4c98c39757653cd4c77
dimple0201 <60427114+dimple0201@users.noreply.github.com>
2021-07-05 20:41:37 +0530

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AuthState from "./context/auth/AuthState";
+
+require("dotenv").config();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthState>
+      <App />
+    </AuthState>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
